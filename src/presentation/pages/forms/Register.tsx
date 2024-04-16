f2aac144f4bbeea874b3d9aa0ef03537062b365c
Ariano2700 <kawuai123loco123@gmail.com>
2024-04-16 00:33:39 -0500

    bg color in login and register update, and home page is update too

--- src/presentation/pages/forms/Register.tsx
+++ src/presentation/pages/forms/Register.tsx
@@ -50,47 +50,49 @@ export const Register = () => {
   };
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <OutSideWrapper>
-          {showErrorDialog && ErrorAlert({ error })}
+      <section className="bg-gray-600 min-h-screen">
+        <div className="flex items-center justify-center h-screen">
+          <OutSideWrapper>
+            {showErrorDialog && ErrorAlert({ error })}
 
-          <form
-            className="flex flex-col items-center gap-6 p-3 w-full"
-            onSubmit={handleSubmit}
-          >
-            <h1 className="text-white font-semibold text-lg md:text-2xl">
-              Registrarse
-            </h1>
-            <InputForm
-              placeholder="Email"
-              type="email"
-              name="email"
-              icon={<SolarUserOutline />}
-              onChange={handleChange}
-            />
-            <InputForm
-              placeholder="Password"
-              type="password"
-              name="password"
-              icon={<SolarLockOutline />}
-              onChange={handleChange}
-            />
-            <div className="text-white text-sm text-right">
-              <Link to={"/"} className="flex cursor-pointer underline">
-                ¿Ya tienes cuenta? Inicia sesión
-              </Link>
-            </div>
-            <div className="flex justify-center items-center gap-4">
-              <button
-                className="bg-green-600 px-7 py-2 text-white rounded-md hover:bg-green-800 transition-all duration-300"
-                type="submit"
-              >
-                Registrar
-              </button>
-            </div>
-          </form>
-        </OutSideWrapper>
-      </div>
+            <form
+              className="flex flex-col items-center gap-6 p-3 w-full"
+              onSubmit={handleSubmit}
+            >
+              <h1 className="text-white font-semibold text-lg md:text-2xl">
+                Registrarse
+              </h1>
+              <InputForm
+                placeholder="Email"
+                type="email"
+                name="email"
+                icon={<SolarUserOutline />}
+                onChange={handleChange}
+              />
+              <InputForm
+                placeholder="Password"
+                type="password"
+                name="password"
+                icon={<SolarLockOutline />}
+                onChange={handleChange}
+              />
+              <div className="text-white text-sm text-right">
+                <Link to={"/"} className="flex cursor-pointer underline">
+                  ¿Ya tienes cuenta? Inicia sesión
+                </Link>
+              </div>
+              <div className="flex justify-center items-center gap-4">
+                <button
+                  className="bg-green-600 px-7 py-2 text-white rounded-md hover:bg-green-800 transition-all duration-300"
+                  type="submit"
+                >
+                  Registrar
+                </button>
+              </div>
+            </form>
+          </OutSideWrapper>
+        </div>
+      </section>
     </>
   );
 };
--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -7,21 +7,30 @@ export const Home = () => {
   const handleLogOut = async () => {
     await logOut();
   };
+
+
   if (loading) return <Loader />;
 
   return (
-    <>
-      <h1>Hola chekes</h1>
-      <h1>{user?.displayName || user?.email}</h1>
-      <button
-        className="text-white px-4 py-2 bg-red-500 mt-4 rounded-md hover:bg-red-800 transition-all duration-300 flex justify-center items-center gap-3"
-        onClick={handleLogOut}
-      >
-        <span>
-          <SolarLogout2Linear />
-        </span>
-        <span>Cerrar sesión</span>
-      </button>
-    </>
+    <main className="bg-black p-8 min-h-screen flex items-center justify-center">
+      <div className=" bg-gray-800 rounded-lg shadow-lg overflow-hidden p-10 h-96 w-96 flex flex-col items-center ">
+        <h1 className="p-2 font-bold mb-12 text-white bg-black rounded-3xl">Hola chekes :V </h1>
+        {user?.photoURL ? (
+          <img src={user.photoURL} alt="User Photo" className=" rounded-full w-24 h-24 animate-bounce" />
+        ) : (
+          <p>No hay foto de usuario disponible</p>
+        )}
+        <h1 className="uppercase text-white font-bold text-2xl">{user?.displayName || user?.email}</h1>
+        <button
+          className="text-white p-4 bg-red-500 mt-4 rounded-md hover:bg-red-800 transition-all duration-300 flex justify-center items-center gap-3"
+          onClick={handleLogOut}
+        >
+          <span>
+            <SolarLogout2Linear />
+          </span>
+          <span>Cerrar sesión</span>
+        </button>
+      </div>
+    </main>
   );
 };
